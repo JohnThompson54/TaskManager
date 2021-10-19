22a52babb02de0e03dbb50c74021b9afa6fc853b
Your Name <you@example.com>
2021-10-19 13:22:29 -0500

    Created the task.js file. Also, setup menu to be displayed.

---
+++ task.js
@@ -0,0 +1,17 @@
+
+
+
+const menu = `TASK MANAGER
+
+What would you like to do (Please enter one of the options below):
+"TASKS" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manager
+
+`;
+
+let userInput = prompt(menu);
+
+
+
